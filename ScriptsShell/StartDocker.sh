6f1d057d41c4ad6e96fabb3647c05dff133321f7
JGMelon22 <jgvasconcellos22@gmail.com>
2022-07-13 22:34:17 -0300

    Melhorando as mensagens dos scripts

--- ScriptsShell/StartDocker.sh
+++ ScriptsShell/StartDocker.sh
@@ -1,1 +1,2 @@
-sudo systemctl start docker && sudo docker start SqlServer1
+# Caso seu container do MSSQL possua um nome diferente, mude onde está escrito "SqlServer1" ;)
+echo "Iniciando o Docker e o SQL Server..." &&sudo systemctl start docker && sudo docker start SqlServer1
--- ScriptsShell/StopDocker.sh
+++ ScriptsShell/StopDocker.sh
@@ -1,1 +1,2 @@
-sudo docker stop SqlServer1 && sudo systemctl stop docker
+# Caso seu container do MSSQL possua um nome diferente, mude onde está escrito "SqlServer1" ;)
+echo "Parando o SQL Server e o Docker..." &&sudo docker stop SqlServer1 && sudo systemctl stop docker
