e64d53690f20961ea81b68129c5bf598d5c65c55
JGMelon22 <jgvasconcellos22@gmail.com>
2022-07-31 13:57:03 -0300

    Alterando os eventos de click dos botões de um genérico para um mais evento mais adequado.

--- EasyGUI/src/MainForm.java
+++ EasyGUI/src/MainForm.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainForm extends JFrame {
     private JButton atualizarSOButton;
@@ -13,41 +15,6 @@ public class MainForm extends JFrame {
     private JButton pararMSSQL;
 
     public MainForm() {
-        atualizarSOButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Atualizar Sistema
-                UpdateUbuntu updateUbuntu = new UpdateUbuntu();
-                updateUbuntu.UpdateSystem();
-                JOptionPane.showMessageDialog(null, "Atualizando o SO.");
-            }
-        });
-        iniciarMSSQL.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                StartStopMSSQL startStopMSSQL = new StartStopMSSQL();
-                startStopMSSQL.Start();
-                JOptionPane.showMessageDialog(null, "Iniciando o SQL Server.");
-            }
-        });
-
-        corrigirAptFlatpak.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                CorrigirPackageManagers corrigirPackageManagers = new CorrigirPackageManagers();
-                corrigirPackageManagers.CorrigirPacotes();
-                JOptionPane.showMessageDialog(null, "Corrigindo pacotes do Apt e/ou Flatpak.");
-            }
-        });
-        pararMSSQL.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                StartStopMSSQL startStopMSSQL = new StartStopMSSQL();
-                startStopMSSQL.Stop();
-                JOptionPane.showMessageDialog(null, "Parando o SQL Server.");
-            }
-        });
-
         // Esconde os botões de iniciar e parar o SQL Server caso marcado
         chkSimple.addActionListener(new ActionListener() {
             @Override
@@ -65,6 +32,39 @@ public class MainForm extends JFrame {
                         pararMSSQL.setVisible(true);
                     }
                 }
+            }
+        });
+        atualizarSOButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Atualizar Sistema
+                UpdateUbuntu updateUbuntu = new UpdateUbuntu();
+                updateUbuntu.UpdateSystem();
+                JOptionPane.showMessageDialog(null, "Atualizando o SO.");
+            }
+        });
+        iniciarMSSQL.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                StartStopMSSQL startStopMSSQL = new StartStopMSSQL();
+                startStopMSSQL.Start();
+                JOptionPane.showMessageDialog(null, "Iniciando o SQL Server.");
+            }
+        });
+        pararMSSQL.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                StartStopMSSQL startStopMSSQL = new StartStopMSSQL();
+                startStopMSSQL.Stop();
+                JOptionPane.showMessageDialog(null, "Parando o SQL Server.");
+            }
+        });
+        corrigirAptFlatpak.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                CorrigirPackageManagers corrigirPackageManagers = new CorrigirPackageManagers();
+                corrigirPackageManagers.CorrigirPacotes();
+                JOptionPane.showMessageDialog(null, "Corrigindo pacotes do Apt e/ou Flatpak.");
             }
         });
     }
